a5a0dfce07bf620c6c855e6d3ddc772a6ffbc80b
Cal McLean <calmcl1@aol.com>
2017-05-31 15:15:52 +0100

    Fixed RX/TX mode selection

--- raftar_start.py
+++ raftar_start.py
@@ -4,17 +4,17 @@
 
 import sys
 import argparse
-import linphone
+#import linphone
 
-from raftar_rx import RAFTaRRX
-from raftar_tx import RAFTaRTX
+#from raftar_rx import RAFTaRRX
+#from raftar_tx import RAFTaRTX
 
 CODECS = ['PCMA', 'PCMU', 'OPUS', 'G722', 'SPEEX']
 #CODECS=['PCMA']
 
 def print_help():
     help_msg = "HCR RAFTaR: (c) Callum McLean 2017 \
-    Usage: raftar_start.py [ rx | tx ] sip_username sip_password"
+    Usage: raftar_start.py sip_username sip_password [ -rx | -tx ]"
 
     print help_msg
 
@@ -24,15 +24,17 @@ if __name__ == "__main__":
     parser.add_argument("sip_password", help="Password for registering on a SIP service for the audio link.")
     
     parser_rx_tx_group = parser.add_mutually_exclusive_group()
-    parser_rx_tx_group.add_argument("rx", type="store_const", dest="mode", value="rx", help="Starts a listening (station-end) instance.")
-    parser_rx_tx_group.add_argument("tx", type="store_const", dest="mode", value="tx", help="Starts a transmitting (field-end) instance.")
+    parser_rx_tx_group.add_argument("-rx", action="store_const", dest="mode", const="rx", help="Starts a listening (station-end) instance.")
+    parser_rx_tx_group.add_argument("-tx", action="store_const", dest="mode", const="tx", help="Starts a transmitting (field-end) instance.")
 
     args = parser.parse_args()
     if args.mode == "rx":
         raf_rx = RAFTaRRX(args.sip_username, args.sip_password, CODECS, 'ALSA: USB Audio Device',
                         'ALSA: USB Audio Device')
         raf_rx.run()
-    elif argsmode == "tx":
+        print args
+    elif args.mode == "tx":
         raf_tx = RAFTaRTX(args.sip_username, args.sip_password, CODECS, 'ALSA: USB Audio Device',
                         'ALSA: USB Audio Device')
         raf_tx.run()
+        print args
